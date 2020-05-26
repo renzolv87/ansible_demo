5e192efbfe681a23836f9e1d9a688c7e5e6fdf69
Renzo <renzo@ansible.com>
2020-05-26 21:33:07 +0200

    borro todo

--- playbooks/playbooks/clamav/files/clamscan_weekly.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-DIR=/
-QUARANTINE_DIR=/var/tmp/clamav_quarantine/
-EXCLUDE_PATHS="/var/lib/lxcfs/cgroup/devices/system/|/sys/module/|/var/lib/waagent/|/var/tmp/clamav_quarantine/|/var/opt/microsoft/"
-
-nice -n 15 clamscan -i -r "$DIR" --exclude-dir="$EXCLUDE_PATHS" --move "$QUARANTINE_DIR" 2>/dev/null
